366c5af7c5a544a7ae2c75a3dc564cf1f285950c
Aashana Shah <aashana.shahh@gmail.com>
2017-11-07 17:22:52 -0800

    Project setup

---
+++ Grocery List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocery List
+//
+//  Created by Aashana on 11/6/17.
+//  Copyright © 2017 Aashana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
